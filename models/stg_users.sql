5452521e5f41b57685d6b71fcc8c197c3f2fc969
Tania Sabzizar <tania.sabzizar@dbtlabs.com>
2024-06-27 09:04:37 +0000

    blind

--- models/stg_users.sql
+++ models/stg_users.sql
@@ -1,5 +1,5 @@
 {{ config(
-    pre_hook="DELETE FROM DBT_TONAYYA.STG_USERS WHERE NAME='JESSICA'"],
+    pre_hook="DELETE FROM DBT_TONAYYA.STG_USERS WHERE NAME='JESSICA'",
 ) }}
 
 Select * from {{ ref('raw_users') }}
