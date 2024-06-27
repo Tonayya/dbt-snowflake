c839f435f7b91464fdc68abfcceff15cf640648f
Tania Sabzizar <tania.sabzizar@dbtlabs.com>
2024-06-27 07:16:25 +0000

    eh

--- models/abc.sql
+++ models/abc.sql
@@ -1,14 +1,1 @@
-With whatever AS (
-    SELECT * FROM {{ ref('foo') }}
-),
-
-investor_type_mapping AS (
-    SELECT * FROM {{ ref('stg_abc') }}
-),
-
-final AS (
-    SELECT * FROM whatever
-    SELECT * FROM investor_type_mapping
-)
-
-SELECT * from final
+select 1 as id
