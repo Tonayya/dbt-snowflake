dc7bf0ed7038d7686cebee42fd833652e7f085a4
Tania Sabzizar <tania.sabzizar@dbtlabs.com>
2024-11-05 01:53:31 +0000

    change incremental model users

--- models/iceberg.sql
+++
@@ -1,9 +0,0 @@
-{{
-    config(
-        materialized = "table",
-        table_format = "iceberg",
-        external_volume = "s3_iceberg_snow",
-    )
-}}
-
-select 1 as id
--- models/users.sql
+++ models/users.sql
@@ -5,3 +5,4 @@
 }}
 
 select 1 id
+-- whatever
